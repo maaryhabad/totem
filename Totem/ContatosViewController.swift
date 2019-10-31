90504f43492b1b5e04dd5d3e90a75fd633269871
Mariana Beilune Abad <maryh.abad@gmail.com>
2019-10-31 11:32:15 -0300

    Salvando usuarios

---
+++ Totem/ContatosViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ContatosViewController.swift
+//  totem
+//
+//  Created by Mariana Beilune Abad on 30/10/19.
+//  Copyright © 2019 Mariana Beilune Abad. All rights reserved.
+//
+
+import UIKit
+
+class ContatosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        addBottomSheetView()
+    }
+    
+    
+    //Adicionar a view
+    func addBottomSheetView() {
+        // 1- Init bottomSheetVC
+        let bottomSheetVC = BottomSheetViewController()
+
+        // 2- Add bottomSheetVC as a child view
+        self.addChild(bottomSheetVC)
+        self.view.addSubview(bottomSheetVC.view)
+        bottomSheetVC.didMove(toParent: self)
+
+        // 3- Adjust bottomSheet frame and initial position.
+        let height = view.frame.height
+        let width  = view.frame.width
+        bottomSheetVC.view.frame = CGRect(x: 0, y: self.view.frame.maxY, width: width, height: height)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Totem/Model/Sentimento.swift
@@ -0,0 +1,9 @@
+//
+//  Sentimento.swift
+//  totem
+//
+//  Created by Mariana Beilune Abad on 30/10/19.
+//  Copyright © 2019 Mariana Beilune Abad. All rights reserved.
+//
+
+import Foundation
