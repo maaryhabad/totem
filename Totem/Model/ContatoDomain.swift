fc2e8f5c80afad7c2ddde060018e6807cc1be080
Mariana Beilune Abad <maryh.abad@gmail.com>
2019-11-08 11:57:10 -0300

    Fluxo da mensagem e melhoria do model

---
+++ Totem/Model/ContatoDomain.swift
@@ -0,0 +1,9 @@
+//
+//  ContatoDomain.swift
+//  totem
+//
+//  Created by Mariana Beilune Abad on 07/11/19.
+//  Copyright © 2019 Mariana Beilune Abad. All rights reserved.
+//
+
+import Foundation
